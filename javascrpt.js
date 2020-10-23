b680fcbe80a3d4058684923443f6b48302d9d271
es <sadiksanhaji@gmail.com>
2020-10-23 15:21:47 +0100

    ""

--- javascrpt.js
+++ javascrpt.js
@@ -2,36 +2,56 @@
 
 let choices = ["rock", "paper", "scissors"];
 // number of rounds the computer won
-let x = 0;
+let computerscore = 0;
 // number of rounds the player have won 
-let y = 0;
+let playerscore = 0;
 // a function that returns a random choice from the choices array
 function computerChoice(){
     let randComChoice = choices[Math.floor( Math.random()* choices.length)];
     return randComChoice;
 }
-let computerplay = computerChoice();
-
-let playerName = prompt("what's your name ?");
-
-let playerChoice = prompt("what's your choice " + playerName);
-console.log(playerChoice);
-
+let computer = computerChoice();
+ let computerplay = computer.toLowerCase()
+let username = prompt("what's your name ?");
+let playerName = username.toLowerCase();
+ function Playerpick(){ 
+let choice = prompt("what's your choice " + playerName);
+ return choice;
+ }
+ let playerChoice = Playerpick();
 function round(){
-if (computerplay == playerChoice){
-  alert(" it's a draw!!!");
-} else  if ( computerplay == choices[0] && playerChoice == choices[1]){
-
-alert("you have won this round ")
-} else  if ( computerplay == choices[1] && playerChoice == choices[2 ]){
-
-alert("you have lost this round ")
-} else  if ( computerplay == choices[2] && playerChoice == choices[0]){
-
-  alert("you have  won this round ")
+   if (computerplay == playerChoice){
+     alert(" it's a draw!!!");
+ }  else  if ( computerplay == choices[0] && playerChoice == choices[1]){
+     alert("you have Won! Paper beats Rock!  ")
+     playerscore += 1;
+ }  else  if ( computerplay == choices[1] && playerChoice == choices[2 ]){
+     alert("you Lose! Scissors cut Paper")
+     computerscore += 1;
+ }  else  if ( computerplay == choices[2] && playerChoice == choices[0]){
+     alert("you have  won! Rock beats Scissors! ")
+     playerscore += 1;
+ }  else {
+     alert("You Lose!")
+     computerscore += 1;
+ }
 }
-}
 
-round();
+ round();
+
+
+ if (confirm("would you like to play a other round??")){
+     round();
+
+ }else {
+
+ }
+
+
+
+
+
+
+
     console.log("the computer have chosen " + computerplay + " the player choice is " + playerChoice);
 
